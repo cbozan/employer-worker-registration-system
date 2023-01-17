3774f6665498352bb3e25c190b4f100730308bdd
cbozan <71611710+cbozan@users.noreply.github.com>
2023-01-17 22:19:02 +0300

    Login page: DB, LoginDAO, Login and Main classes.

---
+++ src/com/cbozan/main/Main.java
@@ -0,0 +1,20 @@
+package com.cbozan.main;
+
+import java.awt.EventQueue;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				
+				new Login();
+				
+			}
+		});
+			
+	}
+	
+}
